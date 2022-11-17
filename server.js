125ac407db173b5ddd0699c35aaa458a8968dc5d
Zachary Cairns <zcairns@email.unc.edu>
2022-11-17 00:56:45 -0500

    fixed api endpoints formatting (req,res)

--- server.js
+++ server.js
@@ -9,25 +9,25 @@ let args = minimist(process.argv.slice(2));
 const app = express();
 const port = 5000 || args.port;
 
-app.get('/app/', req, res) => {
+app.get('/app/', (req, res) => {
 	res.send("200 OK");
-}
+})
 
-app.get('/app/roll', req, res) => {
+app.get('/app/roll', (req, res) => {
 	res.send(roll(6,2,1));
-}
+})
 
-app.get('/app/roll/:sides/', req, res) => {
+app.get('/app/roll/:sides/', (req, res) => {
 	res.send(req.params.sides, 2, 1);
-}
+})
 
-app.get('/app/roll/:sides/:dice/', req, res) => {
+app.get('/app/roll/:sides/:dice/', (req, res) => {
         res.send(req.params.sides, req.params.dice, 1);
-}
+})
 
-app.get('/app/roll/:sides/:dice/:rolls/', req, res) => {
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
 	res.send(req.params.sides, req.params.dice, req.params.rolls);
-}
+})
 
 app.use(function(req,res){
     res.status(404).send("404 NOT FOUND");
