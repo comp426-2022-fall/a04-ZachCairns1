7bf863b521403b55960354382ec016af0ec4c7f7
Zachary Cairns <zcairns@email.unc.edu>
2022-11-17 00:59:35 -0500

    hopefully import finds right lib

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 import express from 'express';
 import minimist from 'minimist';
-import { roll } from 'lib/roll.js';
+import { roll } from './lib/roll.js';
 
 let args = minimist(process.argv.slice(2));
 
