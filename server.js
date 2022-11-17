af861d634fa9df6d11abd51473661b5af39425a2
Zachary Cairns <zcairns@email.unc.edu>
2022-11-17 08:25:42 -0500

    added parseInts because my JSON thought the arguments for roll were strings, not ints

--- server.js
+++ server.js
@@ -16,20 +16,19 @@ app.get('/app/', (req, res) => {
 })
 
 app.get('/app/roll/', (req, res) => {
-	console.log(roll(6,2,1));
 	res.send(roll(6,2,1));
 })
 
 app.get('/app/roll/:sides/', (req, res) => {
-	res.send(roll(req.params.sides, 2, 1));
+	res.send(roll(parseInt(req.params.sides), 2, 1));
 })
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
-        res.send(roll(req.params.sides, req.params.dice, 1));
+        res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
 })
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-	res.send(roll(req.params.sides, req.params.dice, req.params.rolls));
+	res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
 })
 
 app.use(function(req,res){
